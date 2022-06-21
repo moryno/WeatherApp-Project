6ce18614d6a9447e79823af9b36ba11a8d38085c
Maurice <mauricenganga41@gmail.com>
2022-06-21 09:09:42 +0300

    Add an event listener to the form that allows the user to search for a city and submit to be able to see a weather prediction of the search

--- assets/js/index.js
+++ assets/js/index.js
@@ -57,6 +57,72 @@ const uploadDefaultWeather = (weatherData) => {
     `;
 }
 
+const form = document.querySelector("#form");
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit (event){
+    event.preventDefault();
+    const cityInput = event.target.location.value;
+    searchWeather(cityInput);
+    form.reset();
+}
+
+const searchWeather = (city) =>{
+    const options = {
+        method: "GET",
+        headers: {
+            "X-RapidAPI-Key": "d8b0d12ba3msh38e30c06b42d375p1ba990jsne3b6255da171",
+            "X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com"
+        }
+    };
+    
+    fetch(`https://community-open-weather-map.p.rapidapi.com/weather?q=${city}&units=metric`, options)
+        .then(res => res.json())
+        .then(weatherData => renderWeather(weatherData))
+        .catch(err => console.error(err));
+
+}
+
+const renderWeather = (weatherData) => {
+    const defaultDetail = document.querySelector(".wrapper");
+    const weatherDetails = document.querySelector(".weatherDetails")
+
+    const temp = weatherData.main.temp;
+    const city = weatherData.name;
+    const feelsLikeTemp = weatherData.main.feels_like;
+    const cloudy = weatherData.clouds.all;
+    const humidity = weatherData.main.humidity;
+    const windSpeed = weatherData.wind.speed;
+
+    defaultDetail.innerHTML = `
+        <h1 class="degree">${Math.floor(temp)}°</h1>
+        <div class="cityDate">
+        <h1 class="cityName">${city}</h1>
+        <p class="day">13:39-Monday, 9 Sep '19</p>
+        </div>
+    `;
+
+    weatherDetails.innerHTML = `
+        <h3 class="weatherDetailsTitle">Weather Details</h3>
+        <div class="details">
+        <p>Cloudy</p>
+        <p>${cloudy}%</p>
+        </div>
+        <div class="details">
+        <p>Humidity</p>
+        <p>${humidity}%</p>
+        </div>
+        <div class="details">
+        <p>Wind</p>
+        <p>${windSpeed}m/s</p>
+        </div>
+        <div class="details">
+        <p>Feels Like</p>
+        <p>${feelsLikeTemp}<strong>°</strong></p>
+        </div>
+    `;
+}
+
 
 init();
 
